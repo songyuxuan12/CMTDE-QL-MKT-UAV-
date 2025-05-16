2f0a4ca575cb2a0e4e15619af4960449c7430e40
songyuxuan12 <125028907+songyuxuan12@users.noreply.github.com>
2025-05-16 17:45:09 +0800

    Update QL.m

--- CMTDE-QL-MKT/QL.m
+++ CMTDE-QL-MKT/QL.m
@@ -1,6 +1,6 @@
 function [Q_table]=QL(Q_table,aidtask_rewards,task_index,EvBestFitness,generation,aid_index,q_index,gen,t_num)
  
-%×Ô¶¨Òå²ÎÊı
+
 a=1-(0.9*(generation/gen));
 x=0.8;
 
@@ -9,18 +9,18 @@ x=0.8;
 
 i=task_index;
 
-%±¾´Îstate
+%æœ¬æ¬¡state
 q_index1=q_index;
 
-%Õâ¸öÊÇnext_state
-if EvBestFitness(i,generation)==EvBestFitness(i,generation-1) %Í³¼ÆÓĞ¼¸´úÏİÈë¾Ö²¿×îÓÅ
+%è¿™ä¸ªæ˜¯next_state
+if EvBestFitness(i,generation)==EvBestFitness(i,generation-1)
     q_index2=(i-1)*2+2;
 else
     q_index2=(i-1)*2+1;
 end
 
 
-%¸üĞÂQ±í
+%æ›´æ–°Qè¡¨
 [maxq,~]=max(Q_table(q_index2,:));
 r=aidtask_rewards(aid_index);
 % Q_table(q_index1,aid_index)=Q_table(q_index1,aid_index)+a*[r+x*(maxq-Q_table(q_index1,aid_index))];
