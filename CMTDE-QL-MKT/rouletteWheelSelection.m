d854a6ba85087f8845635a832cf8b3055f0f52c2
songyuxuan12 <125028907+songyuxuan12@users.noreply.github.com>
2025-05-16 17:07:44 +0800

    Update rouletteWheelSelection.m

--- CMTDE-QL-MKT/rouletteWheelSelection.m
+++ CMTDE-QL-MKT/rouletteWheelSelection.m
@@ -1,27 +1,20 @@
 function selected_indices = rouletteWheelSelection(probability, num_to_select)  
-    % 输入：  
-    % fitnesses 
-    % population_size - 种群大小，即fitnesses数组的长度  
-    % num_to_select - 需要选择的个体数量  
-      
-    % 输出：  
-    % selected_indices - 被选中的个体的索引数组  
-%     [~,population_size]=size(fitnesses); 
-    % 归一化适应度  
+
+
     normalized_probability = probability ./ sum(probability);  
       
-    % 计算累积概率  
+
     cumulative_probs = cumsum(normalized_probability);  
       
-    % 初始化被选中的个体索引数组  
+
     selected_indices = zeros(1, num_to_select);  
       
-    % 轮盘赌选择  
+
     for i = 1:num_to_select  
-        % 生成一个[0, 1)之间的随机数  
+
         r = rand();  
 %          disp(r); 
-        % 查找随机数对应的索引  
+
         [~, idx] = min(abs(cumulative_probs - r));  
           
         % 保存选中的索引  
