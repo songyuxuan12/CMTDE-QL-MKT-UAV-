b837e66c3283df55748e14220d121a557d761ddc
songyuxuan12 <125028907+songyuxuan12@users.noreply.github.com>
2025-05-16 17:44:52 +0800

    Update MAIN.m

--- CMTDE-QL-MKT/MAIN.m
+++ CMTDE-QL-MKT/MAIN.m
@@ -3,14 +3,14 @@ tic
 
 clc
 
-pop_M=100; % 种群大小
-gen=100; % 
-p_il = 0; %无局部搜索
-p_cr=0.9;  %交叉概率
-F=0.5;   %突变比例因数
-B=0.4;  %避免冷启动的参数
-total_case=1; %多任务问题总数量
-repetition=1; %重复运行次数
+pop_M=100;
+gen=100; 
+p_il = 0; 
+p_cr=0.9; 
+F=0.5;   
+B=0.4; 
+total_case=1; 
+repetition=1;
 results_record=zeros(repetition,15);% 4 case 乘以组件数量
 for i=1:repetition
     for j=1:total_case
