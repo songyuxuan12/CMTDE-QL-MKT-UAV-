360eca8f7f02cd836803cddac57ccfe480c91e51
songyuxuan12 <125028907+songyuxuan12@users.noreply.github.com>
2025-05-16 17:07:59 +0800

    Update Chromosome.m

--- CMTDE-QL-MKT/Chromosome.m
+++ CMTDE-QL-MKT/Chromosome.m
@@ -2,7 +2,7 @@ classdef Chromosome
     properties
         rnvec; % (genotype)--> decode to find design variables --> (phenotype) 
         factorial_costs;
-        population_source;%便于统计留下来的解来自于哪个源任务种群
+        population_source;
     end    
     methods        
         function object = initialize(object,D) 
@@ -22,13 +22,13 @@ classdef Chromosome
             %calls = funcCount;
         end
         
-%          function object=crossover(object,p1,p2,cf)     %交叉
+%          function object=crossover(object,p1,p2,cf)     %浜ゅ弶
 %             object.rnvec=0.5*((1+cf).*p1.rnvec + (1-cf).*p2.rnvec);
 %             object.rnvec(object.rnvec>1)=1;
 %             object.rnvec(object.rnvec<0)=0;
 %         end
 %         
-%         function object=mutate(object,p,D,sigma)       %突变
+%         function object=mutate(object,p,D,sigma)       %绐佸彉
 %             rvec=normrnd(0,sigma,[1,D]);
 %             object.rnvec=p.rnvec+rvec;
 %             object.rnvec(object.rnvec>1)=1;
